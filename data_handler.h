418a3c5752a1761135deac2be15ad3f49fe38515
Abdul Ghani <itsaghani@gmail.com>
2024-10-05 14:09:02 +0500

    Issue #4 DONE

---
+++ data_handler.h
@@ -0,0 +1,11 @@
+#ifndef DATA_HANDLER_H
+#define DATA_HANDLER_H
+
+struct Data {
+    int number;
+};
+
+struct Data show_whole_data();
+
+#endif // DATA_HANDLER_H
+
--- main.c
+++ main.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "get_console.h"
+#include "main.h"
+#include "data_handler.h"
 
+void repeat_char(char to_print, int size);
+void main_menu(int user_choice);
 
-void repeat_char(char,int);
 int main(void) {
-  int console_size = get_console_size();
-  printf("Your console is %d spaces wide.\n" , console_size);
-  repeat_char('=' , console_size);
-  return 0;
+    int console_size = get_console_size();
+    printf("Your console is %d spaces wide.\n", console_size);
+    repeat_char('=', console_size);
+    main_menu(1);
+    return 0;
 }
 
-
-void repeat_char(char to_print,int size){
-  for(int i=0;i<size;i++){
-    printf("%c" , to_print);
-  }
-  printf("\n");
+void repeat_char(char to_print, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%c", to_print);
+    }
+    printf("\n");
 }
+
+void main_menu(int user_choice) {
+    switch (user_choice) {
+        case 1:
+            show_whole_data();
+            break;
+        default:
+            exit(1);
+    }
+}
+
